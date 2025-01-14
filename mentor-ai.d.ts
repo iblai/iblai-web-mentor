2b149ec2a3f1768f01805cd9362e70188bb6ed6d
Sone Gillis <sonegillis@gmail.com>
2025-01-14 03:07:39 +0100

    fixes mentor-ai

---
+++ mentor-ai.d.ts
@@ -0,0 +1,23 @@
+export interface MentorAIElement extends HTMLElement {
+  mentorUrl?: string;
+  authUrl?: string;
+  tenant: string;
+  mentor: string;
+  isAnonymous?: boolean;
+  isAdvanced?: boolean;
+  isContextAware?: boolean;
+  redirectToken?: string;
+}
+declare global {
+  interface HTMLElementTagNameMap {
+    "mentor-ai": MentorAIElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "mentor-ai": Partial<MentorAIElement> &
+        React.HTMLAttributes<MentorAIElement>;
+    }
+  }
+}
+
+export {};
